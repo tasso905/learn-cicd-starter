c7ff6d09550b60d4a855c3e53e4bd3d6cd0cb6c4
tasso905 <tasso75@hotmail.com>
2025-05-22 13:56:45 -0400

    fixed gosec errors

--- json.go
+++ json.go
@@ -30,5 +30,10 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+	if err != nil {
+		// Handle the error - perhaps log it
+		log.Printf("Error writing response: %v", err)
+	}
+
 }
